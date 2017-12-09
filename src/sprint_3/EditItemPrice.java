57dd9f1396f945c1de52934c914b3c9ca3ff6be3
kahyeelow <yeelow76@gmail.com>
2017-12-09 17:41:06 +0800

    Second Submission. Author: Low Kah Yee

--- src/sprint_3/EditItemPrice.java
+++ src/sprint_3/EditItemPrice.java
@@ -1,10 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sprint_3;
 
+//Author
 import java.util.ArrayList;
 import java.util.Scanner;
 import sprint_3.Service;
