c70b2e48aa03a1aa84e476c796e4f0a89d6e92f0
ng <ng@192.168.1.6>
2017-12-09 18:30:31 +0800

    [Finish #152590627] First Submission Author: Ng Li Poh

---
+++ src/sprint_3/DeliveryManInfo.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sprint_3;
+
+/**
+ *
+ * @author ng
+ */
+public class DeliveryManInfo {
+      private String regNo,deliveryManName,ic, phoneNo,email,address,deliveryStatus,workingStatus;
+
+    public DeliveryManInfo() {
+    }
+
+    public DeliveryManInfo(String regNo, String deliveryManName, String ic, String phoneNo, String email, String address, String deliveryStatus, String workingStatus) {
+        this.regNo = regNo;
+        this.deliveryManName = deliveryManName;
+        this.ic = ic;
+        this.phoneNo = phoneNo;
+        this.email = email;
+        this.address = address;
+        this.deliveryStatus = deliveryStatus;
+        this.workingStatus = workingStatus;
+    }
+
+    public String getRegNo() {
+        return regNo;
+    }
+
+    public String getDeliveryManName() {
+        return deliveryManName;
+    }
+
+    public String getIc() {
+        return ic;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDeliveryStatus() {
+        return deliveryStatus;
+    }
+
+    public String getWorkingStatus() {
+        return workingStatus;
+    }
+
+    public void setRegNo(String regNo) {
+        this.regNo = regNo;
+    }
+
+    public void setDeliveryManName(String deliveryManName) {
+        this.deliveryManName = deliveryManName;
+    }
+
+    public void setIc(String ic) {
+        this.ic = ic;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setDeliveryStatus(String deliveryStatus) {
+        this.deliveryStatus = deliveryStatus;
+    }
+
+    public void setWorkingStatus(String workingStatus) {
+        this.workingStatus = workingStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "DeliveryManInfo{" + "regNo=" + regNo + ", deliveryManName=" + deliveryManName + ", ic=" + ic + ", phoneNo=" + phoneNo + ", email=" + email + ", address=" + address + ", deliveryStatus=" + deliveryStatus + ", workingStatus=" + workingStatus + '}';
+    }
+   
+}
---
+++ src/sprint_3/ViewDeliveryStatus.java
@@ -0,0 +1,121 @@
+package sprint_3;
+
+//Author: Ng Li Poh
+import java.util.*;
+
+public class ViewDeliveryStatus {
+
+    public static void main(String[] args) {
+        ArrayList<DeliveryManInfo> deliInfo = new ArrayList<>();
+
+        DeliveryManInfo worker1 = new DeliveryManInfo("W101", "Alex Chan", "970417-10-6124", "0123456789", "abc92@mail.com", "3,jalan aman,taman aman", "Break", "Resigned");
+        DeliveryManInfo worker2 = new DeliveryManInfo("W102", "Benny Fan", "121221-33-1251", "0198765432", "fedc98@gmail.com", "22,prima setapak condo", "Available", "Retired");
+        DeliveryManInfo worker3 = new DeliveryManInfo("W103", "Polly Ng", "980530-09-1234", "0158749632", "quada99@mailcom", "99,jalan bunga wangi,taman wangi", "Delivering", "Employed");
+        DeliveryManInfo worker4 = new DeliveryManInfo("W104", "Kah Yee Fan", "971017-10-6121", "0123456789", "abc92@mail.com", "3,jalan aman,taman aman", "Delivering", "Employed");
+        DeliveryManInfo worker5 = new DeliveryManInfo("W105", "Liaw Swee Tian", "120921-33-1224", "0198765432", "fedc98@gmail.com", "22,prima setapak condo", "Delivering", "Employed");
+        DeliveryManInfo worker6 = new DeliveryManInfo("W106", "Jason Chong", "980430-09-1234", "0158749632", "quada99@mailcom", "99,jalan bunga wangi,taman wangi", "Break", "Resigned");
+        DeliveryManInfo worker7 = new DeliveryManInfo("W107", "Ali Binti Shadin", "971217-10-5363", "0123456789", "abc92@mail.com", "3,jalan aman,taman aman", "Available", "Employed");
+        DeliveryManInfo worker8 = new DeliveryManInfo("W108", "Muthu", "121221-33-3251", "0198765432", "fedc98@gmail.com", "22,prima setapak condo", "Available", "Employed");
+        DeliveryManInfo worker9 = new DeliveryManInfo("W109", "Chaw Rong Weng", "980730-09-1234", "0158749632", "quada99@mailcom", "99,jalan bunga wangi,taman wangi", "Available", "Resigned");
+
+        deliInfo.add(worker1);
+        deliInfo.add(worker2);
+        deliInfo.add(worker3);
+        deliInfo.add(worker4);
+        deliInfo.add(worker5);
+        deliInfo.add(worker6);
+        deliInfo.add(worker7);
+        deliInfo.add(worker8);
+        deliInfo.add(worker9);
+
+        Scanner scan = new Scanner(System.in);
+        title();
+
+        do {
+            System.out.println("---------------Menu--------------------");
+            System.out.println("1. View Delivery Man's Working Status");
+            System.out.println("2. Update Delivery Man's Status");
+            System.out.println("3. Update Delivery Man Information");
+            System.out.print("\nPlease select your option: ");
+            Integer input = scan.nextInt();
+            if (input == 1) {
+                System.out.println("\nDo you want to view all delivery man's working status or search by ID?");
+                System.out.println("1. View All");
+                System.out.println("2. Search by ID");
+                System.out.print("Please enter your selection: ");
+                input = scan.nextInt();
+                //if is 1 then show all,if 2 can search by ID
+                switch (input) {
+                    case 1:
+                        viewStatus();
+                        for (int i = 0; i < deliInfo.size(); ++i) {
+
+                            System.out.println("" + deliInfo.get(i).getRegNo() + "        " + String.format("%-20s", deliInfo.get(i).getDeliveryManName()) + String.format("%-20s", deliInfo.get(i).getWorkingStatus()));
+
+                        } //if input is 2,search by ID
+                        break;
+                    case 2:
+                        System.out.print("Please enter the ID of the delivery man: ");
+                        String regNo = scan.next();
+                        int a=1;
+                        for (int i = 0; i < deliInfo.size(); ++i) {
+                            if (regNo.equals(deliInfo.get(i).getRegNo())) {
+                                viewStatus();
+                                System.out.println("" + deliInfo.get(i).getRegNo() + "        " + String.format("%-20s", deliInfo.get(i).getDeliveryManName()) + String.format("%-20s", deliInfo.get(i).getWorkingStatus()));
+                                a=0;
+                            }
+                            
+                        }
+                         if(a==1)
+                                   System.out.println("Sorry, the ID does not exist in the list. Please try again."); 
+                        break;
+                    default:
+                        System.out.println("The number is out of range! Plese enter a valid number.");
+                        break;
+                }
+
+            } else {
+                System.out.println("The number is out of range! Plese enter a valid number.");
+            }
+
+        } while (confirmation()
+                == 0);
+        System.out.println(
+                "Thanks for using the system. Have a Nice Day :)");
+
+    }
+
+    public static void title() {
+        System.out.println("\n=============================================");
+        System.out.println("         FASTEST DELIVERY SDN.BHD");
+        System.out.println("=============================================\n");
+
+    }
+
+    public static void viewStatus() {
+        title();
+
+        System.out.println("        Delivery Man's Working Status ");
+        System.out.println("-------------------------------------------------");
+        System.out.println("ID          Name                  Status");
+        System.out.println("-------------------------------------------------");
+
+    }
+
+    public static int confirmation() {
+        Scanner scan = new Scanner(System.in);
+        int exit = 0;
+        System.out.print("\nDo you want to continue(Y/N)? ");
+        String con = scan.next();
+
+        char con1 = con.charAt(0);
+
+        if (con1 == 'Y' || con1 == 'y') {
+            exit = 0;
+        } else {
+            exit = 1;
+        }
+        return exit;
+    }
+
+}
